160c9cbe29a6a0b675073f4a86f42915a55f2603
myportfolios <lexybiade@gmail.com>
2020-11-30 23:32:39 -0500

    converted yemperature unit to metric

--- utils/getWeatherByGeoLocationWithAxios.js
+++ utils/getWeatherByGeoLocationWithAxios.js
@@ -3,7 +3,7 @@ const apiKey = "210a56654019e4f42b67b2a316a4d3bd"
 
 
 const axiosGetWeatherByGeolocation = ({longitude, latitude, location}, callBack) => {
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${longitude}&lon=${latitude}&appid=${apiKey}`
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
 //check if geolocation was provided
 if(!(longitude && latitude)){
     return callBack("No geolocation details provided!")
